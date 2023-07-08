73c81ad219957b22af517151e4adbbc48c616d2b
Abhishek Adhikari <abhishek.adhikari276@gmail.com>
2023-07-08 14:08:14 +0545

    add user to DB

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
 const express = require('express');
+const serverConfig = require('./configs/server.config');
+const mongoose = require('mongoose');
+const dbConfig = require('./configs/db.config');
+const userModel = require('./models/user.model');
 
 const app = express();
 
-const PORT = 3030;
+// Logic to connect to MongoDB and create an ADMIN user 
+mongoose.connect(dbConfig.DB_URL);
+const db = mongoose.connection;
 
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
+
+
+db.on("error", () => {
+    console.log('error while connecting to DB');
+})
+
+db.once("open", () => {
+    console.log('database is connected');
+
+    init();
+
+})
+
+const init = async() => {
+
+    let admin = await userModel.findOne({
+        userId : "admin"
+    });
+
+    if(admin) {
+        console.log('admin user already present');
+        return;
+    };
+
+    admin = await userModel.create({
+        name: "Abhishek Adhikari",
+        userId: "admin",
+        email: "admin@email.com",
+        userType: "ADMIN",
+        password: "12345"
+    });
+
+    console.log(admin);
+}
+
+
+app.listen(serverConfig.PORT, () => {
+    console.log(`server started on port ${serverConfig.PORT}`)
 });
